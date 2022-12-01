2b7c64fa6db8af24b855c9879c50302a460fe62f
Hayley Walters <hwalters@g.hmc.edu>
2022-12-01 14:09:50 -0800

    part 2 answer

--- day-1.py
+++ day-1.py
@@ -1,14 +1,18 @@
 def main():
-    max_sum = 0
     temp_sum = 0
+    top_3 = [0,0,0]
     for line in open('day-1.txt', 'r'):
         if line == "\n":
-            max_sum = temp_sum if (max_sum < temp_sum) else max_sum
+            if min(top_3) < temp_sum:
+                top_3.remove(min(top_3))
+                top_3.append(temp_sum)
             temp_sum = 0
         else:
             temp_sum += int(line)
-    print(max_sum)
-
+    # Part 1 answer output (Expected 69626)
+    print(f'Max cals: {top_3[0]}')
+    # Part 2 answer output (Expected 206780)
+    print(f'Sum top 3: {sum(top_3)}')
 
 if __name__ == "__main__":
     main()
